2b6d446c1218f033e510b6da6cb053c141db99de
Sean Mcintyre <sean_mcintyre@hotmail.com>
2023-08-08 13:34:17 -0400

    more cleanup

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet"></link>
     <App />
   </React.StrictMode>
 );
